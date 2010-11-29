2106ed860b8a0c9830eef5ffdc0ffb7d10df8211
Cyril Adrian <cyril.adrian@gmail.com>
2010-11-29 08:16:59 +0100

    alpha channel, logging

--- src/net/cadrian/microcosmos/main.py
+++ src/net/cadrian/microcosmos/main.py
@@ -15,48 +15,73 @@
 #
 import logging
 import os
+import pygame
 
 from pysge.controllers.scores import Scores
 from pysge.core.application import Application
+from pysge.core.engine import SpriteEngine
 from pysge.dataModel.scores import ScoresData
 from pysge.facade import PySGE
-from pysge.utils.logger import getLogger, setupTestLogging
+from pysge.utils.logger import consoleHandler, fileHandler, getLogger, setupTestLogging
 
 from net.cadrian.microcosmos.controller import LevelController
 from net.cadrian.microcosmos.menus import ChoiceMenuController, MainMenuController
 
+
+formatter = logging.Formatter("%(asctime)s | %(thread)d | %(name)-40s | %(levelname)-8s | %(message)s")
+consoleHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+
+
 _LOGGER = getLogger(__name__)
+
+
+class MySpriteEngine(SpriteEngine):
+    def loadImage(self, resource):
+        def convertImage(filename):
+            surface = pygame.image.load(filename).convert_alpha()
+            #surface.set_colorkey(TRANSPARENCY, pygame.RLEACCEL)
+            return surface
+
+        return self._loadResource(resource, convertImage).copy()
+
 
 class Game(Application):
     def __init__(self):
-	#setupTestLogging(logging.DEBUG)
-	setupTestLogging(logging.INFO)
-	configPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/game.ini")
-	Application.__init__(self, configPath)
+        #setupTestLogging(logging.DEBUG)
+        setupTestLogging(logging.INFO)
+        configPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/game.ini")
+        Application.__init__(self, configPath)
+        PySGE.engine = MySpriteEngine(
+            PySGE.configuration.getScreenSize(),
+            PySGE.configuration.getName(),
+            PySGE.configuration.getResourcePath(),
+            PySGE.eventManager
+        )
 
     def setUp(self):
-	self._levels = []
-	PySGE.objectFactory.registerModules("net.cadrian.microcosmos.widgets")
-	PySGE.setScoreHandler(ScoresData, PySGE.configuration.getScoresPath())
-	self._makeLevels()
-	self._makeMenus()
+        self._levels = []
+        PySGE.objectFactory.registerModules("net.cadrian.microcosmos.widgets")
+        PySGE.setScoreHandler(ScoresData, PySGE.configuration.getScoresPath())
+        self._makeLevels()
+        self._makeMenus()
 
     def _makeLevels(self):
-	levelNames = [levelPath for levelPath in os.listdir(PySGE.configuration.getLevelPath()) if levelPath.endswith(".data")]
-	for level in levelNames:
-	    levelName, ext = level.split('.')
-	    levelController = LevelController(os.path.join(PySGE.configuration.getLevelPath(), level))
-	    self._screens[levelName] = levelController
-	    self._levels.append(levelName)
-	self._levels.sort()
-	_LOGGER.info("Known levels are: %s", self._levels)
+        levelNames = [levelPath for levelPath in os.listdir(PySGE.configuration.getLevelPath()) if levelPath.endswith(".data")]
+        for level in levelNames:
+            levelName, ext = level.split('.')
+            levelController = LevelController(os.path.join(PySGE.configuration.getLevelPath(), level))
+            self._screens[levelName] = levelController
+            self._levels.append(levelName)
+        self._levels.sort()
+        _LOGGER.info("Known levels are: %s", self._levels)
 
     def _makeMenus(self):
-	def makeMenu(factory, name, *args):
-	    return factory(os.path.join(PySGE.configuration.getBasePath(), name), *args)
+        def makeMenu(factory, name, *args):
+            return factory(os.path.join(PySGE.configuration.getBasePath(), name), *args)
 
-	self._screens['firstScreen'] =  makeMenu(MainMenuController, "screens/mainMenu.data")
-	self._screens['choose'] = makeMenu(ChoiceMenuController, "screens/choiceMenu.data", self._levels)
-	#self._screens['victory'] = makeMenu(MenuController, "screens/victoryMenu.data")
-	#self._screens['defeat'] = makeMenu(MenuController, "screens/defeatMenu.data")
-	#self._screens['scores'] = makeMenu(Scores, "screens/scores.data")
+        self._screens['firstScreen'] =  makeMenu(MainMenuController, "screens/mainMenu.data")
+        self._screens['choose'] = makeMenu(ChoiceMenuController, "screens/choiceMenu.data", self._levels)
+        #self._screens['victory'] = makeMenu(MenuController, "screens/victoryMenu.data")
+        #self._screens['defeat'] = makeMenu(MenuController, "screens/defeatMenu.data")
+        #self._screens['scores'] = makeMenu(Scores, "screens/scores.data")
